3169ce10801172d830448fadeafc52f26c82785a
mdmokchhedulislam <mokchheduls46@gmail.com>
2026-01-27 16:53:24 +0600

    update vpc like production ready

--- infra/module/vpc/main.tf
+++ infra/module/vpc/main.tf
@@ -1,53 +1,104 @@
-# VPC
-resource "aws_vpc" "project_vpc" {
+
+resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "${var.project_name}-vpc"
+    Name = var.project_name
   }
 }
 
-# Public Subnets
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+
 resource "aws_subnet" "public" {
   for_each = var.public_subnets
 
-  vpc_id                  = aws_vpc.project_vpc.id
+  vpc_id                  = aws_vpc.this.id
   cidr_block              = each.value.cidr
   availability_zone       = each.value.az
   map_public_ip_on_launch = true
 
   tags = {
-    Name                     = "${var.project_name}-public-${each.key}"
-    "kubernetes.io/role/elb" = "1"
+    Name = "${var.project_name}-public-${each.key}"
   }
 }
 
-# Private Subnets
 resource "aws_subnet" "private" {
   for_each = var.private_subnets
 
-  vpc_id            = aws_vpc.project_vpc.id
+  vpc_id            = aws_vpc.this.id
   cidr_block        = each.value.cidr
   availability_zone = each.value.az
 
   tags = {
-    Name                              = "${var.project_name}-private-${each.key}"
-    "kubernetes.io/role/internal-elb" = "1"
+    Name = "${var.project_name}-private-${each.key}"
   }
 }
 
-# NAT Gateway
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public[keys(var.public_subnets)[0]].id
-  tags          = { Name = "${var.project_name}-nat-gw" }
+
+resource "aws_eip" "nat" {
+  for_each = aws_subnet.public
+  domain   = "vpc"
 }
 
-# Route Table Association 
+resource "aws_nat_gateway" "this" {
+  for_each = aws_subnet.public
+
+  allocation_id = aws_eip.nat[each.key].id
+  subnet_id     = each.value.id
+
+  tags = {
+    Name = "${var.project_name}-nat-${each.key}"
+  }
+}
+
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+
 resource "aws_route_table_association" "public" {
-  for_each  = aws_subnet.public
-  subnet_id = each.value.id
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
   route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_route_table" "private" {
+  for_each = aws_subnet.private
+
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this[each.key].id
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-rt-${each.key}"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  for_each = aws_subnet.private
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private[each.key].id
 }
